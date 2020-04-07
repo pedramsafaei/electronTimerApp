4cade5cca70aea258b75956fe72a2788680e914c
redps <psafaei@nevada.unr.edu>
2020-04-07 01:25:30 -0700

    Fix for #3

--- angular-electron/src/app/app.module.ts
+++ angular-electron/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'; \
-import { FormsModule } from '@angular/forms'
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
 
 export class YourModule {}
