0f4a108775fb535eb12ec45886b840acf1c39d27
redps <psafaei@nevada.unr.edu>
2020-04-06 22:52:31 -0700

    some progress on timer app

--- angular-electron/src/app/app.component.ts
+++ angular-electron/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-electron';
+  title = 'Timer App';
+  max = 1;
+  current = 0;
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
 }
